8dbe74dd458aa4ab9465567f99450d1c1de63638
CinematicCow <26029731+CinematicCow@users.noreply.github.com>
2023-10-30 21:38:21 +0530

    feat: add models
    constants for lumora

---
+++ internal/models/lumora.go
@@ -0,0 +1,6 @@
+package models
+
+type Lumora struct {
+	Key   string
+	Value string
+}
---
+++ internal/models/paths.go
@@ -0,0 +1,5 @@
+package models
+
+const DIR_PATH = "$HOME/.lumora/"
+const FILE_PATH = "lumora.gob"
+const LUMORA_PATH = DIR_PATH + FILE_PATH
