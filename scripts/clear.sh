5f25436ed229336a204bcffb2a9e1c9197bd17ed
CinematicCow <26029731+CinematicCow@users.noreply.github.com>
2023-10-30 20:13:57 +0530

    feat: add init & clear scripts

---
+++ scripts/clear.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Get the directory to delete
+directory="$HOME/.config/lumora"
+
+# Check if the user wants to bypass the prompt
+if [[ "$1" == "-y" ]]; then
+  # Delete the directory
+  rm -rf "$directory"
+  echo "Lumora has been wiped clean."
+else
+  # Prompt the user for confirmation
+  read -p "Are you sure you want to delete the '$directory' directory? (y/N) " answer
+
+  # Check the user's response
+  if [[ "$answer" == "y" ]]; then
+    # Delete the directory
+    rm -rf "$directory"
+    echo "Lumora has been wiped clean."
+  else
+    # Exit the script
+    echo "Lumora is intact."
+    exit 1
+  fi
+fi
---
+++ scripts/init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Set the directory and file names.
+dir_name="$HOME/.config/lumora"
+file_name="lumora"
+
+# Check if the directory or file already exists.
+if [[ -d "$dir_name" || -f "$dir_name/$file_name" ]]; then
+  # Ask the user if they want to overwrite the directory or file.
+  echo "The directory or file already exists. Do you want to overwrite it? (y/n)"
+  read overwrite
+
+  # If the user says yes, delete the directory or file and create a new one.
+  if [[ "$overwrite" == "y" ]]; then
+    rm -rf "$dir_name"
+    mkdir -p "$dir_name"
+    touch "$dir_name/$file_name"
+    echo "Lumora has been initialized."
+  else
+    echo "Lumora could not be initialized."
+  fi
+else
+  # Create the directory and file.
+  mkdir -p "$dir_name"
+  touch "$dir_name/$file_name"
+  echo "Lumora has been initialized."
+fi
+
+# Exit with the OK code.
+exit 0
