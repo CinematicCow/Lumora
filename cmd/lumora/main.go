310cfb0208c5c30fea2d4b041be0387cc782bb00
CinematicCow <26029731+CinematicCow@users.noreply.github.com>
2023-10-31 13:53:50 +0530

    chore: test manual

--- cmd/lumora/main.go
+++ cmd/lumora/main.go
@@ -11,26 +11,37 @@ import (
 
 func main() {
 	fmt.Println("Hello, Lumora!")
+
 	expandedPath := os.ExpandEnv(models.LUMORA_PATH)
+
 	db, err := os.Open(expandedPath)
 	if err != nil {
-		log.Panicf("something went wrong: %v", err)
+		log.Panicf("something went wrong at main in opening file: %v", err)
 	}
 
-	if err := database.AddToDB(db, models.Lumora{
-		Key:   "test",
-		Value: "test",
-	}); err != nil {
+	defer db.Close()
+
+	dbForWrite, err := os.Create(expandedPath)
+
+	if err != nil {
+		log.Panicf("something went wrong at dbForWrite: %v", err)
+	}
+
+	defer dbForWrite.Close()
+
+	Key := []byte("hello1")
+	Value := []byte("world2")
+
+	if err := database.AddToDB(dbForWrite, &Key, &Value); err != nil {
 		log.Panicf("something went wrong at add: %v", err)
 	}
 
 	data, err := database.GetAllFromDB(db)
 
 	if err != nil {
-		log.Panicf("something went wrong: %v", err)
+		log.Panicf("something went wrong at main getAll: %v", err)
 	}
 
 	fmt.Print(data)
-	defer db.Close()
 
 }
